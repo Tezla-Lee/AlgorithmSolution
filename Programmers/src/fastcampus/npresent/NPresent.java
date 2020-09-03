e8c85acd6ee2e1334ba64aa2fe9bc45d5259070a
Tezla-Lee <tezla.lee@gmail.com>
2020-09-03 10:36:56 +0900

    Update

--- Programmers/src/fastcampus/npresent/NPresent.java
+++ Programmers/src/fastcampus/npresent/NPresent.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+// https://programmers.co.kr/learn/courses/30/lessons/42895
+
 public class NPresent {
     public int solution(int N, int number) {
         List<Set<Integer>> sets = new ArrayList<>();
@@ -22,18 +24,18 @@ public class NPresent {
             for (int j = 1; j < i / 2 + 1; j++) {
                 for (int x : sets.get(j)) {
                     for (int y : sets.get(i - j)) {
-//                        if (x + y > 0) {
+                        if (x + y > 0) {
                             set.add(x + y);
-//                        }
-//                        if (x * y > 0) {
+                        }
+                        if (x * y > 0) {
                             set.add(x * y);
-//                        }
-//                        if (x - y > 0) {
+                        }
+                        if (x - y > 0) {
                             set.add(x - y);
-//                        }
-//                        if (y - x > 0) {
+                        }
+                        if (y - x > 0) {
                             set.add(y - x);
-//                        }
+                        }
                         if (y != 0) {
                             set.add(x / y);
                         }
@@ -47,10 +49,12 @@ public class NPresent {
             if (set.contains(number)) {
                 return i;
             }
+//            System.out.println(set.size() + "개 -> " + set);  // Print Set for Check
         }
         return -1;
     }
 
+    // TDD
     public static void main(String[] args) {
         System.out.println(new NPresent().solution(7, 7));
         System.out.println(new NPresent().solution(7, 6666));
